43b191b5fdb2a1c227510bd01f2676567495d2ca
Robert Luttrell <reluttre@calpoly.edu>
2021-03-02 22:28:52 -0800

    Added driver code to test_perf_curve.m

---
+++ test_perf_curve.m
@@ -0,0 +1,20 @@
+figure();
+hold on;
+
+omega_arr = [3000:1000:6000];
+
+for idx = 1:length(omega_arr)
+   omega = omega_arr(idx);
+   [h, q] = get_perf_curve(omega);
+   plot(q, h, "LineWidth", 2);
+end
+
+xlim([0, 5500]);
+ylim([0, 10]);
+
+legendCell = cellstr(num2str(omega_arr', 'omega=%-d'));
+legend(legendCell, "Location", "Northwest");
+
+% legend("3000 rpm", "4000 rpm", "5000 rpm", "6000 rpm");
+
+saveas(gcf, "fan_curves.png");
