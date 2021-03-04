5c6bd3cd3eb8910a561f6d44490f0d72aca5a073
Robert Luttrell <reluttre@calpoly.edu>
2021-03-03 22:01:43 -0800

    implemented h vs q for based on omega - not tested

--- get_perf_curve_affinity.m
+++ get_perf_curve_affinity.m
@@ -1,98 +1,133 @@
 function [h, q] = get_perf_curve(omega)
 
-   q_provided = [1760,
-                 2200,
-                 2640,
-                 3080,
-                 3520,
-                 3960,
-                 4400,
-                 4840];  % cfm
+   q_provided = [8900,
+                 13350,
+                 17800,
+                 22250,
+                 26700,
+                 31150,
+                 35600,
+                 40050,
+                 44500,
+                 48950,
+                 53400];  % updated
+                 
 
-   rpm_05 = [2119,
-             2562,
-             3016,
-             3477,
-             3943,
-             4411,
-             4882,
-             5354];
+   rpm_05 = [371,
+             484,
+             606,
+             733,
+             863,
+             995,
+             1128,
+             1262,
+             1396,
+             1531,
+             1667];  % updated
 
-   rpm_1 = [2301,
-            2715,
-            3147,
-            3592,
-            4044,
-            4502,
-            4964,
-            5430];
+   rpm_1 = [449,
+            543,
+            655,
+            775,
+            900,
+            1027,
+            1157,
+            1288,
+            1420,
+            1553,
+            1687];  % updated
 
-   rpm_2 = [2615,
-            2994,
-            3394,
-            3809,
-            4239,
-            4678,
-            5125,
-            5577];
 
-   rpm_3 = [2880,
-            3240,
-            3619,
-            4015,
-            4425,
-            4847,
-            5279,
-            5719];
+   rpm_2 = [NaN,
+            649,
+            743,
+            851,
+            967,
+            1088,
+            1212,
+            1338,
+            1466,
+            1595,
+            1726];  % updated
 
-   rpm_4 = [3112,
-            3461,
-            3826,
-            4207,
-            4603,
-            5011,
-            5430,
-            5858];
+   rpm_3 = [NaN,
+            747,
+            823,
+            920,
+            1029,
+            1143,
+            1263,
+            1385,
+            1509,
+            1636,
+            1763];  % updated
 
-   rpm_5 = [3323,
-            3660,
-            4017,
-            4387,
-            4771,
-            5169,
-            5576,
-            5994];
 
-   rpm_6 = [3514,
-            3845,
-            4194,
-            4556,
-            4931,
-            5320,
-            5719,
-            6127];
 
-   rpm_7 = [3700,
-            4021,
-            4360,
-            4717,
-            5084,
-            5464,
-            5856,
-            6257];
+   rpm_4 = [NaN,
+            NaN,
+            898,
+            986,
+            1087,
+            1196,
+            1310,
+            1429,
+            1551,
+            1674,
+            1800];  % updated
 
-   rpm_8 = [3891,
-            4183,
-            4517,
-            4869,
-            5231,
-            5604,
-            5989,
-            6384];
+   rpm_5 = [NaN,
+            NaN,
+            972,
+            1048,
+            1142,
+            1246,
+            1356,
+            1472,
+            1590,
+            1712,
+            1835];  % updated
+
+   rpm_6 = [NaN,
+            NaN,
+            1044,
+            1108,
+            1195,
+            1294,
+            1401,
+            1513,
+            1629,
+            1748,
+            1869];  % updated
+
+   rpm_7 = [NaN,
+            NaN,
+            NaN,
+            1167,
+            1247,
+            1341,
+            1444,
+            1553,
+            1666,
+            1783,
+            1902];  % updated
+
+   rpm_8 = [NaN,
+            NaN,
+            NaN,
+            1226,
+            1297,
+            1387,
+            1486,
+            1592,
+            1702,
+            1817,
+            1934];  % updated
 
    sp = [0.5, 1, 2, 3, 4, 5, 6, 7, 8]'; % in H2O
    rpm_vects = [rpm_05, rpm_1, rpm_2, rpm_3, rpm_4, rpm_5, rpm_6, rpm_7, rpm_8]';
    q = zeros(length(sp), 1);
+   h = zeros(length(sp), 1);
 
 
    conversion_omega = 0.01667;  % rps / rpm
@@ -108,6 +143,8 @@ function [h, q] = get_perf_curve(omega)
       cur_rpm_vect = rpm_vects(i, :)';
       [min_value, closest_index] = min(abs(omega - cur_rpm_vect));
       closest_omega = cur_rpm_vect(closest_index);
+      display(closest_index);
+      display(cur_rpm_vect);
 
       % Get closest Q
       closest_q_cfm = q_provided(closest_index);  % cfm
@@ -124,13 +161,15 @@ function [h, q] = get_perf_curve(omega)
       rho_air_std = 0.075;  % lbm / ft^3
       rho_H2O_std = 62.4;  % lbm / ft^3
       rho_air_metric = rho_air_std * conversion_rho;  % kg / m^3
-      sp_inH2O = sp[closest_index];  % in H2O
+      sp_inH2O = sp(closest_index);  % in H2O
       sp_metric = sp_inH2O * conversion_sp;  % Pa
-      h_a_m_air = sp_Pa / (rho_air_metric * g_metric) + (1 + K_L) * ( (vbar_fan_metric)^2 / (2 * g_metric) );  % m air
+      h_a_m_air = sp_metric / (rho_air_metric * g_metric) + (1 + K_L) * ( (vbar_fan_metric)^2 / (2 * g_metric) );  % m air
       h_a_in_air = h_a_m_air * in_per_m;  % in air
       closest_h_a_in_H2O = h_a_in_air * (rho_air_std / rho_H2O_std);
 
-
+      % Use affinity laws to get H at actual omega
+      new_h_a = (omega / closest_omega) ^ 2 * closest_h_a_in_H2O;  % in H2O
+      h(i) = new_h_a;  % in H2O
 
    end
 
