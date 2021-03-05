064a92e1d2355546624db153d1bcf1dc017f5ed3
Robert Luttrell <reluttre@calpoly.edu>
2021-03-04 20:17:54 -0800

    Fan curve function working

--- get_perf_curve_affinity.m
+++ get_perf_curve_affinity.m
@@ -134,7 +134,7 @@ function [h, q] = get_perf_curve(omega)
    conversion_q = 0.000472;  % m^3/s / cfm
    conversion_A = 0.0929;  % m^3 / ft^3
    conversion_rho = 16.018;  % kg/m^3 / lbm/ft^3
-   conversion_sp = 0.00024836;  % Pa / in H2O
+   conversion_sp = 248.36;  % Pa / in H2O
    g_metric = 9.81;  % m / s^2
    in_per_m = 39.37;  % in / m
    K_L = 0.2;
@@ -143,11 +143,10 @@ function [h, q] = get_perf_curve(omega)
       cur_rpm_vect = rpm_vects(i, :)';
       [min_value, closest_index] = min(abs(omega - cur_rpm_vect));
       closest_omega = cur_rpm_vect(closest_index);
-      display(closest_index);
-      display(cur_rpm_vect);
 
       % Get closest Q
       closest_q_cfm = q_provided(closest_index);  % cfm
+      closest_q_metric = closest_q_cfm * conversion_q;  % m^3 / s
 
       % Use affinity laws to get Q at actual omega
       new_q = (omega / closest_omega) * closest_q_cfm;  % cfm
@@ -156,12 +155,11 @@ function [h, q] = get_perf_curve(omega)
       % Get closest actual head rise
       A_fan_ft2 = 8.90;  % ft^2
       A_fan_metric = A_fan_ft2 * conversion_A;  % m^2
-      closest_q_metric = closest_q_cfm * conversion_q;  % m^3 / s
       vbar_fan_metric = closest_q_metric / A_fan_metric;  % m / s
       rho_air_std = 0.075;  % lbm / ft^3
       rho_H2O_std = 62.4;  % lbm / ft^3
       rho_air_metric = rho_air_std * conversion_rho;  % kg / m^3
-      sp_inH2O = sp(closest_index);  % in H2O
+      sp_inH2O = sp(i);  % in H2O
       sp_metric = sp_inH2O * conversion_sp;  % Pa
       h_a_m_air = sp_metric / (rho_air_metric * g_metric) + (1 + K_L) * ( (vbar_fan_metric)^2 / (2 * g_metric) );  % m air
       h_a_in_air = h_a_m_air * in_per_m;  % in air
